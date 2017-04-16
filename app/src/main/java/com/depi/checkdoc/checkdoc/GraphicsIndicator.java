d505dca29894426afb2b3b1e4a15b9ec5573ce73
Carlos Clavero <c.clavero74@gmail.com>
2017-04-16 18:21:28 +0200

    Arreglando algunas cosillas

--- app/src/main/java/com/depi/checkdoc/checkdoc/GraphicsIndicator.java
+++ app/src/main/java/com/depi/checkdoc/checkdoc/GraphicsIndicator.java
@@ -1,10 +1,12 @@
 package com.depi.checkdoc.checkdoc;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -24,6 +26,33 @@ public class GraphicsIndicator extends AppCompatActivity {
         setContentView(R.layout.activity_graphics_indicator);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //          Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //                  .setAction("Action", null).show();
+
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(GraphicsIndicator.this);
+                builder1.setMessage(getResources().getString(R.string.doctorWarn));
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        getResources().getString(R.string.accept),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+            }
+        });
+
+
 
         TextView title = (TextView) findViewById(R.id.Titlegraphic);
         bundle = this.getIntent().getExtras();
--- app/src/main/java/com/depi/checkdoc/checkdoc/History.java
+++ app/src/main/java/com/depi/checkdoc/checkdoc/History.java
@@ -1,9 +1,11 @@
 package com.depi.checkdoc.checkdoc;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -41,11 +43,25 @@ public class History extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //                .setAction("Action", null).show();
+                //          Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //                  .setAction("Action", null).show();
+
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(History.this);
+                builder1.setMessage(getResources().getString(R.string.doctorWarn));
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        getResources().getString(R.string.accept),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
             }
         });
-
 
 
 //...
