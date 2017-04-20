b6f1ea8d46eace5f49d2564d8e5f1db73772583f
acardoco <andres.cardosoc12@gmail.com>
2017-04-21 01:39:30 +0200

    Chat. No se llama a la actividad desde ningún sitio (no se si meterla en ajustes, o en el menú directamente, en el contacto con un doctor, ...).

--- app/src/main/java/com/depi/checkdoc/checkdoc/Chat.java
+++ app/src/main/java/com/depi/checkdoc/checkdoc/Chat.java
@@ -1,14 +1,20 @@
 package com.depi.checkdoc.checkdoc;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Chat extends AppCompatActivity {
+
+    ImageButton ib;
+    TextView outputText;
+    EditText inputText;
+    String texto = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,21 @@ public class Chat extends AppCompatActivity {
         //Esto pone el título en la barra de arriba cada vez
         TextView txtTitle = (TextView) findViewById(R.id.txtAbTitulo);
         txtTitle.setText(getResources().getString(R.string.title_activity_chat));
+
+        ib = (ImageButton)findViewById(R.id.imageButton);
+        inputText = (EditText) findViewById(R.id.textoInput);
+        outputText = (TextView)findViewById(R.id.textoOutput);
+        outputText.setKeyListener(null);//para no hacerlo editable
+
+        ib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto = texto + Html.fromHtml("<br />") + inputText.getText();
+                outputText.setText(texto);
+                inputText.getText().clear();
+            }
+        });
+
     }
 
 }
